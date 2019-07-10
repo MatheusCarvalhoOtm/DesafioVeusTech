a12619f2ff840edba3c48603a0e9bae1525a9a0b
MatheusCarvalhoOtm <42623531+MatheusCarvalhoOtm@users.noreply.github.com>
2019-07-10 18:39:16 -0300

    Update desafio_veus.c

--- desafio_veus.c
+++ desafio_veus.c
@@ -19,12 +19,9 @@ int main ()
 {
   char input[5]; //recebe o valor do usuário através do stdin
   int aux[4]; //armazena todos os dados até o buffer estar completo
-
   
 
       while(1){
-    
-    
           for (int i = 0; i < 5; i++){
           gets(input);
     	  aux[i] = arraycharparaint (input);
@@ -32,7 +29,6 @@ int main ()
          // printf ("\n");
           printf ("RDWCV=[%.1f],", (float) aux[4] / 100);
           printf ("RDWSD=[%.1f]\r", (float) aux[0] / 100);
-    
          // printf ("MCV= [%d],", (int) aux[1]); não utilizado no desafio
          //  printf ("MCHC= [%d],", (int) aux[2]);  não utilizado no desafio
          // printf ("MCH= [%d],", (int) aux[3]);  não utilizado no desafio
